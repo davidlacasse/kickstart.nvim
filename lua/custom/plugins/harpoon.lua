bfb271ba13cfee5d13a12363b391adeb45c80cfe
dlacasse <dlacasse@genetec.com>
2025-01-08 18:27:52 -0500

    add new stuff

--- lua/custom/plugins/harpoon.lua
+++ lua/custom/plugins/harpoon.lua
@@ -6,11 +6,13 @@ return {
   },
   config = true,
   keys = {
-    { '<leader>a', "<cmd>lua require('harpoon.mark').add_file()<cr>", desc = 'Mark file with harpoon' },
+    { '<leader>aa', "<cmd>lua require('harpoon.mark').add_file()<cr>", desc = 'Mark file with harpoon' },
     { '<leader>pa', '<cmd>Telescope harpoon marks<CR>', desc = 'Show harpoon marks' },
+    { '<leader>am', '<cmd>lua require("harpoon.ui").toggle_quick_menu()<CR>', desc = 'Toggle Harpoon menu' },
     { '<C-S-j>', '<cmd>lua require("harpoon.ui").nav_file(1)<CR>', desc = 'Show harpoon marks' },
     { '<C-S-k>', '<cmd>lua require("harpoon.ui").nav_file(2)<CR>', desc = 'Show harpoon marks' },
     { '<C-S-l>', '<cmd>lua require("harpoon.ui").nav_file(3)<CR>', desc = 'Show harpoon marks' },
+    { '<C-S-;>', '<cmd>lua require("harpoon.ui").nav_file(4)<CR>', desc = 'Show harpoon marks' },
     { '<C-S-;>', '<cmd>lua require("harpoon.ui").nav_file(4)<CR>', desc = 'Show harpoon marks' },
   },
 }
